81d59117dab1f5cedf3401c5ac167f4b85e3bae5
Jovani <202226019_gonzalez@tesch.edu.mx>
2026-01-19 21:01:53 -0600

    Update pacientes.js

--- js/pacientes.js
+++ js/pacientes.js
@@ -2,10 +2,12 @@ let pacientes = JSON.parse(localStorage.getItem("pacientes")) || [];
 
 function agregarPaciente() {
     const nombre = document.getElementById("nombrePaciente").value;
+
     if (nombre === "") return;
 
     pacientes.push(nombre);
     localStorage.setItem("pacientes", JSON.stringify(pacientes));
+
     document.getElementById("nombrePaciente").value = "";
     mostrarPacientes();
 }
@@ -14,18 +16,18 @@ function mostrarPacientes() {
     const lista = document.getElementById("listaPacientes");
     lista.innerHTML = "";
 
-    pacientes.forEach((p, i) => {
+    pacientes.forEach((paciente, i) => {
         lista.innerHTML += `
             <li>
-                ${p}
+                ${paciente}
                 <button onclick="eliminarPaciente(${i})">Eliminar</button>
             </li>
         `;
     });
 }
 
-function eliminarPaciente(index) {
-    pacientes.splice(index, 1);
+function eliminarPaciente(i) {
+    pacientes.splice(i, 1);
     localStorage.setItem("pacientes", JSON.stringify(pacientes));
     mostrarPacientes();
 }
